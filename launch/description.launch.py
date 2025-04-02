ec26de6cadadb0bce43bd6752458c70f39edce74
northpoleforce <northpoleforce@qq.com>
2025-04-02 14:05:12 +0800

    fix rviz cannot display: modify leg.xacro add joint_state_publisher_node

--- launch/description.launch.py
+++ launch/description.launch.py
@@ -12,6 +12,7 @@ def generate_launch_description():
 
     use_sim_time = LaunchConfiguration("use_sim_time")
     description_path = LaunchConfiguration("description_path")
+    urdf_path = LaunchConfiguration("urdf_path")
 
     declare_use_sim_time = DeclareLaunchArgument("use_sim_time", default_value="false", description="Use simulation (Gazebo) clock if true")
 
@@ -20,24 +21,36 @@ def generate_launch_description():
 
     declare_description_path = DeclareLaunchArgument(name="description_path", default_value=default_model_path, description="Absolute path to robot urdf file")
 
+    default_urdf_path = os.path.join(pkg_share, "urdf/robot.urdf")
+    declare_urdf_path = DeclareLaunchArgument(name="urdf_path", default_value=default_urdf_path, description="Absolute path to robot urdf file")
+
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         
         parameters=[
             {"robot_description": Command(["xacro ", description_path])},
-            {"use_tf_static": False},
+            # {"robot_description": Command(["cat ", urdf_path])},
+            # {"use_tf_static": False},
             {"publish_frequency": 200.0},
             {"ignore_timestamp": True},
             {'use_sim_time': use_sim_time}
             ],
         # remappings=(("robot_description", "robot_description")),
     )
+    
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        parameters=[{"use_sim_time": use_sim_time}],
+    )
 
     return LaunchDescription(
         [
             declare_description_path,
+            declare_urdf_path,
             declare_use_sim_time,
             robot_state_publisher_node,
+            joint_state_publisher_node,
         ]
     )
